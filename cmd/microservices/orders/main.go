924fdf9d00a023ae46c664f86866d296af881bd7
amrremam <erorat10@gmail.com>
2024-10-05 22:30:48 +0300

    updating code

--- cmd/microservices/orders/main.go
+++ cmd/microservices/orders/main.go
@@ -1,1 +1,45 @@
 package orders
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("Starting orders microservice")
+
+	ctx := cmd.Context()
+
+	r, closeFn := createOrdersMicroservice()
+	defer closeFn()
+
+	server := &http.Server{Addr: os.Getenv("SHOP_ORDERS_SERVICE_BIND_ADDR"), Handler: r}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Closing orders microservice")
+
+	if err := server.Close(); err != nil {
+		panic(err)
+	}
+}
+
+
+func createOrderMicroservice() (router *chi.Mux, closeFn func()) {
+	cmd.WaitForService(os.Getenv("SHOP_RABBITMQ_ADDR"))
+	shopHTTPClient := orders_infra_product.NewHTTPClient(os.Getenv("SHOP_SERVICE_ADDR"))
+
+	r := cmd.CreateRouter()
+
+	orders_public_http.AddRoutes(r, ordersService, ordersRepo)
+	orders_private_http.AddRoutes(r, ordersService, ordersRepo)
+
+	return r, func(){
+
+	}
+}
--- cmd/microservices/shop/main.go
+++ cmd/microservices/shop/main.go
@@ -1,1 +1,44 @@
 package shop
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("Starting shop microservice")
+
+	ctx := cmd.Context()
+
+	r := createShopMicroservice()
+	server := &http.Server{Addr: os.Getenv("SHOP_SHOP_SERVICE_BIND_ADDR"), Handler: r}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Closing shop microservice")
+
+	if err := server.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func createShopMicroservice() *chi.Mux {
+	shopProductRepo := shop_infra_product.NewMemoryRepository()
+	shopProductsService := shop_app.NewProductsService(shopProductRepo, shopProductRepo)
+
+	if err := shop.LoadShopFixtures(shopProductsService); err != nil {
+		panic(err)
+	}
+
+	r := cmd.CreateRouter()
+
+	shop_interfaces_public_http.AddRoutes(r, shopProductRepo)
+	shop_interfaces_private_http.AddRoutes(r, shopProductRepo)
+
+	return r
+}
